ad41df1bc538ddbceba3bd77235bce7d7775ea94
princenedjoh <princenedjoh5@gmail.com>
2023-06-18 01:05:41 +0000

    update

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: returns 0
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		putchar(l);
+	}
+	for (l = 'A'; l <= 'Z'; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
