1e7015a1b581dbd2a9235ec1992eb529444432e2
princenedjoh <princenedjoh5@gmail.com>
2023-06-20 13:12:57 +0000

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,8 +5,13 @@
  * Return: returns 0
  */
 
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+		_putchar('\n');
+	}
 }
