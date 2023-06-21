3b9eb660f143980689ef0f74063cb7e33217f56f
princenedjoh <princenedjoh5@gmail.com>
2023-06-21 02:47:12 +0000

    update

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - entry point
+ * @n: param
+ * Return: returns 0
+ */
+
+int print_last_digit(int n)
+{
+	int lastDigit;
+
+	lastDigit = n % 10;
+	return (lastDigit);
+}
