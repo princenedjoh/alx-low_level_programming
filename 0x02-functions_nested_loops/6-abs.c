49b758e358c1b9a9f03a293f3d61155d8eae3ddd
princenedjoh <princenedjoh5@gmail.com>
2023-06-21 02:34:51 +0000

    update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _abs - entry point
+ * Return: returns 0
+ */
+
+int _abs(int n)
+{
+	int value;
+
+	if (n < 0)
+	{
+		value = n - (n * 2);
+		return (value);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (n);
+	}
+
+}
