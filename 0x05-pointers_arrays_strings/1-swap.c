39380500ce15f408f3e8394c4015abbb1850a18c
princenedjoh <princenedjoh5@gmail.com>
2023-06-26 23:50:33 +0000

    update

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+/**
+ * void swap_int(int *a, int *b) - entry point
+ * @a: parameter
+ * @b: parameter
+ * swap: integer
+ */
+
+void swap_int(int *a, int *b)
+{
+	int swap;
+
+	swap = *a;
+	*a = *b;
+	*b = swap;
+}
