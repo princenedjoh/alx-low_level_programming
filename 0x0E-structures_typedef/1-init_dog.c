bad18e26c5466a65511a41456f480f0fc9c44bde
princenedjoh <princenedjoh5@gmail.com>
2023-07-17 19:16:33 +0000

    update

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+
+/**
+ * main - main function
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+	return (0)
+}
