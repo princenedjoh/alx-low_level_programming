00dc7b9b2b00b44ba93ed55b364887964436bdd9
princenedjoh <princenedjoh5@gmail.com>
2023-07-17 15:12:05 +0000

    update

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - entry point
  * Return: returns 0
  */
 
-void main(void)
+int main(void)
 {
-	printf("%s\n", __FILE__)
-	return (0)
+	printf("%s\n", __FILE__);
+	return (0);
 }
