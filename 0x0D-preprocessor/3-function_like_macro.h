b0ab39d5552da7c69885bb4defc8a43e574755b0
princenedjoh <princenedjoh5@gmail.com>
2023-07-17 15:31:52 +0000

    update

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,5 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
-#define ABS(x) ((x) < 0 ? printf("%d\n", 2*(x)+(x)) : printf("%d\n", (x)))
+#define ABS(x) ((x) < (0) ? (2*(x)+(x)) : (x))
 
 #endif
